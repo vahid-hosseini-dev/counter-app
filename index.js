2a2e56a56a6cdb5020a4225e60ff8d77125adfe4
Vahid Hosseini <vhdnet@gmail.com>
2023-12-09 23:39:47 +0330

    some change in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementById("count").innerText = 5;
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+  console.log(count);
+}
