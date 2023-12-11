80dec59e31911f0049bebdf7f5d44d3ec87ad2eb
vhdent <vhdnet@gmail.com>
2023-12-11 17:07:00 +0330

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let countEl = document.getElementById("count-el");
-
 let count = 0;
+let saveEl = document.getElementById("save-el");
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
-  console.log(count);
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
